8b4135003adca52edb91c29f69aab6db5fe4a652
Steven Ng <steven.ng@braintapper.com>
2020-12-17 12:22:44 -0500

    Cleanup, version bump

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-export { default as LeftSplit }
+export { default as LayoutLeftSplit }
 from './LeftSplit.svelte';
-export { default as OutlineView }
+export { default as LayoutOutlineView }
 from './OutlineView.svelte';
-export { default as TopSplit }
+export { default as LayoutTopSplit }
 from './TopSplit.svelte';
